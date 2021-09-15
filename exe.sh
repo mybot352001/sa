e6c613cdcd604b9fa399352edae49eab8d959154
kzinthant-kas <88583919+kzinthant-kas@users.noreply.github.com>
2021-09-15 15:58:23 +0630

    Add files via upload

---
+++ exe.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip3 install -r requirements.txt
+python3 gensa.py --quick-setup $1
+python3 createmail.py
+zip -q accounts.zip accounts/
+zip -q all.zip accounts.zip token.pickle email.txt
