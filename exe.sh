d768e601be1ecba1c554d31da68aa99e2481137c
kzinthant-kas <88583919+kzinthant-kas@users.noreply.github.com>
2021-09-16 22:23:15 +0630

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -3,6 +3,6 @@ pip3 install -r requirements.txt
 sleep 5
 python3 gensa.py --quick-setup $1
 python3 createmail.py
-zip -q accounts.zip accounts/
+zip -q accounts.zip accounts/*
 mv token_sa.pickle token.pickle
 zip -q all.zip accounts.zip token.pickle email.txt
